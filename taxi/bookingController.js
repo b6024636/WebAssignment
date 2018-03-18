e2087f2bdda62db561e662d3a8d1f1c290519bca
b6024636 <b6024636@my.shu.ac.uk>
2018-03-18 17:33:48 +0000

    Log in and booking
    Added in the login log out feature, now needs styling and commenting

---
+++ taxi/bookingController.js
@@ -0,0 +1,156 @@
+angular.module("taxiModule").controller("bookingController", function ($scope, $http) {
+    $scope.bookings = [];
+    $scope.routes = [];
+	$scope.vehicles = [];
+	var bookingId;
+
+	//Recieves the callBookings broadcast and runs the load function
+    $scope.$on('callBookings', function () {
+        $scope.showBookingList();
+       
+    });
+	//Hides booking content
+    $scope.$on('hideContent', function () {
+        $scope.hideBookings();
+    });
+	
+	//Populates url with data from url
+    $scope.loadBookings = function () {
+        $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking")
+            .success(function (response) {
+                $scope.bookings = response;
+
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            });
+    };
+	
+	//Shows the current bookings
+    $scope.showBookingList = function () {
+        $scope.showCurrentBookings = true;
+		$scope.loadBookings();
+        $scope.isBookingAdding = false;
+		$scope.isBookingEditing = false;
+		$scope.resetBookingForm();
+		
+    };
+
+    
+	//Shows the add screen and loads the current vehicles and routes
+    $scope.beginBookingAdding = function () {
+        $scope.isBookingAdding = true;
+        $scope.showCurrentBookings = false;
+		//Current routes
+		$http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/route")
+            .success(function (response) {
+                $scope.routes = response;
+
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            });
+		//Current vehicles
+		$http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/vehicle")
+            .success(function (response) {
+                $scope.vehicles = response;               
+            })
+            .error(function (error) {                
+                $scope.errorMessage = error;
+            });
+    };
+
+	//Hides the booking content
+    $scope.hideBookings = function () {
+        $scope.showCurrentBookings = false;
+		$scope.isBookingAdding = false;
+		$scope.isBookingEditing = false;
+    };
+	
+	//Sets all the inputs blank
+	$scope.resetBookingForm = function () {
+		$scope.addBookingDropOff = "";
+		$scope.addBookingPickup = "";
+		$scope.addBookingNoPassengers = "";
+		$scope.addBookingName = "";
+		$scope.addBookingVehicleId = "";
+		$scope.editBookingDropOff = "";
+		$scope.editBookingPickup = "";
+		$scope.editBookingNoPassengers = "";
+		$scope.editBookingVehicleId = "";
+	};
+
+	//Pushes the new details to the array
+    $scope.addBooking = function () {
+        var bookingDetails = {
+			DropOffLocation: $scope.addBookingDropOff,
+			PickupLocation: $scope.addBookingPickup,
+			CurrentPassenger: $scope.addBookingNoPassengers,
+			PassengerName: $scope.addBookingName,
+			VehicleId: $scope.addBookingVehicleId
+        };
+		//Posts the inputed booking details to the url array (Id auto increments, isn't needed)
+        $http.post("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking", bookingDetails)
+            .success(function () {				
+                $scope.showBookingList();     
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            }); 
+    };
+	
+	//Goes to the show booking view
+    $scope.cancelBooking = function () {
+        $scope.showBookingList();
+    };
+	
+	$scope.removeBooking = function (bookingId) {
+		$http.delete("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking/" + bookingId.Id)
+            .success(function () {
+                $scope.loadBookings()
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            })
+	};
+	
+	//Gets the selected booking id for editing and displays the response in the form
+    $scope.editBooking = function (id) {
+        $scope.isBookingEditing = true;
+        $scope.showCurrentBookings = false;
+        $http.get("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking/" + id)
+            .success(function (response) {
+                bookingId = response.Id;
+                $scope.editBookingDropOff = response.DropOffLocation;
+				$scope.editBookingPickup = response.PickupLocation;
+				$scope.editBookingNoPassengers = response.CurrentPassenger;
+				$scope.BookingName = response.PassengerName;
+				$scope.editBookingVehicleId = response.VehicleId;
+            })
+            .error(function (error) {
+                $scope.errorMessage = error;
+            });
+            
+    };
+	
+	//Similar to the add function, this takes the new edited inputs and updates the existing record using its Id
+    $scope.editBookingPush = function () {
+        var editedBookingDetails = {
+			Id: bookingId,
+            DropOffLocation: $scope.editBookingDropOff,
+			PickupLocation: $scope.editBookingPickup,
+			CurrentPassenger: $scope.editBookingNoPassengers,
+			PassengerName: $scope.editBookingName,
+			VehicleId: $scope.editBookingVehicleId
+        };
+
+        $http.put("http://webteach_net.hallam.shu.ac.uk/acesjas/api/booking", editedBookingDetails)
+            .success(function (response) {
+                $scope.showBookingList();
+            })
+            .error(function (error) {
+                $scope.errorMessage;
+            });
+    };
+
+});
---
+++ taxi/pageController.js
@@ -0,0 +1,81 @@
+angular.module("taxiModule").controller("pageController", function ($scope, $http) {
+
+	$scope.loginButton = true;
+	
+    //Displays the main navigation element broadcasts a message to other controllers to hide their content
+    $scope.returnHome = function () {   
+		if($scope.isAuthenticated == true) {
+			$scope.$broadcast('hideContent');
+			$scope.showHomeNavigation = true;
+			$scope.loginForm = true;	
+			$scope.currentUser = true;
+		}        
+    };
+
+
+    //Hides main navigation, shows the current taxis
+    $scope.viewTaxis = function () {
+        $scope.showHomeNavigation = false;
+        //Broadcast used to send message taxiController so that the http.get function can be run
+        $scope.$broadcast('callTaxis');
+    };
+
+    $scope.viewBookings = function () {
+        $scope.showHomeNavigation = false;
+        $scope.$broadcast('callBookings');
+    };
+
+    $scope.viewRoutes = function () {
+        $scope.showHomeNavigation = false;
+        $scope.$broadcast('callRoutes');
+    }
+	
+	//Login//
+	
+	    // *** VARIABLE DEFINITIONS *** //
+    $scope.name = "";
+    $scope.isAuthenticated = false;
+    $scope.role = 0;
+
+
+   
+
+    // *** FUNCTION DEFINITIONS *** //
+    $scope.login = function () {
+        var authenticationDetails = {
+            username: $scope.username,
+            password: $scope.password,
+        };
+        $http.post("http://webteach_net.hallam.shu.ac.uk/acesjas/api/login", authenticationDetails)
+        .success(function (response) {
+            $scope.name = response.name;
+            $scope.isAuthenticated = response.authenticated;
+            $scope.role = response.role;
+						if($scope.isAuthenticated == true) {
+							$scope.loginButton = false;
+							$scope.logout = false;
+							$scope.returnHome();
+							$scope.loginError = false;
+							$scope.username = "";
+							$scope.password = "";				
+                             
+						} else{
+                           $scope.loginError = true;
+                       }
+        })
+        .error(function (error) {
+            $scope.errorMessage = error;
+        });
+    };
+	
+	$scope.logout = function (){
+		$scope.name = "";
+		$scope.isAuthenticated = false;
+		$scope.role = 0;
+		$scope.returnHome();
+		$scope.showHomeNavigation = false;
+		$scope.currentUser = false;
+	}
+
+
+});
